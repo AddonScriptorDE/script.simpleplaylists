dd9e17b230987d0ff59909984fd8accf6485d90b
Hans Wurst <addonscriptorde@yahoo.de>
2012-06-11 00:21:34 +0200

    Version 1.0.8

--- remove.py
+++ remove.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 import sys,os,re,urllib,xbmcplugin,xbmcaddon,xbmcgui
 
-addonID = "plugin.video.watch.it.later"
+addonID = "script.simpleplaylists"
 settings = xbmcaddon.Addon(id=addonID)
 useAlternatePlaylistPath=settings.getSetting("useAlternatePlDir")
 
@@ -12,16 +12,28 @@ else:
   myPlaylist=xbmc.translatePath("special://profile/addon_data/"+addonID+"/SimplePlaylists.pl")
 
 spl=urllib.unquote_plus(sys.argv[1]).split(":::")
+pl=""
 mode=spl[0]
 playlistEntry=spl[1]
+if playlistEntry.find(";;;")>=0:
+  pl=playlistEntry.split(";;;")[1]
+  playlistEntry=playlistEntry.split(";;;")[0]
 
 newContent=""
-if mode=="removeFromPlaylist" or mode=="removePlaylist":
-  fh = open(myPlaylist, 'r')
-  for line in fh:
-    if line.find(playlistEntry)==-1:
+fh = open(myPlaylist, 'r')
+for line in fh:
+  if mode=="removePlaylist":
+    if line.find(playlistEntry+"###")==-1:
        newContent+=line
-  fh.close()
+  elif mode=="removeFromPlaylist":
+    if line.find(playlistEntry)>=0 and line.find("###PLAYLIST###="+pl+"###END###")>=0:
+      pass
+    else:
+      newContent+=line
+  elif mode=="removeAllPlaylists":
+    if line.find("###PLAYLIST###="+playlistEntry)==-1:
+       newContent+=line
+fh.close()
 fh=open(myPlaylist, 'w')
 fh.write(newContent)
 fh.close()
